994b3aad7a23e82edd4d3d05ac27c9fc3ba322d9
6Neel9 <88641136+6Neel9@users.noreply.github.com>
2022-10-30 22:47:57 +0530

    Add files via upload

---
+++ client1/src/components/body/home/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+    return (
+        <div className="home_page">
+            <h2>Hello everyone!</h2>
+            <p>
+                This site is about user authentication, 
+                so there won't be any other pages here. 
+                If people want to see more about how to 
+                create other websites. You can click on 
+                the link below, visit youtube. 
+            </p>
+            <a href="https://www.youtube.com" target="_blank" 
+            rel="noopener noreferrer">Youtube</a>
+
+
+        </div>
+    )
+}
+
+export default Home
